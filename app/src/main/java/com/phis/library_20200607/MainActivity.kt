2a4036e70d0d0bda4a5c6f0a0b837364ad9a701d
parkcrystal <9jeworld@naver.com>
2020-06-07 11:52:31 +0900

    Glide로 웹상의 이미지 불러서 출력
     - 인터넷 사용 권한 명시

--- app/src/main/java/com/phis/library_20200607/MainActivity.kt
+++ app/src/main/java/com/phis/library_20200607/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -29,6 +30,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        Glide.with(mContext).load("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwingbling.co.kr%2Farticle%2Fwingbling-x-celeb%2F13%2F224460%2F&psig=AOvVaw2o1iMH_bQ36H5KwYgpaIa_&ust=1591584413006000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDV2I7Y7ukCFQAAAAAdAAAAABAb").into(profileImg)
     }
 
 
